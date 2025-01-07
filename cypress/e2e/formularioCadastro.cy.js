49b99633ab86f74a6320e3b63a758aea4d9ccfb4
mclarar <mariac.ribeiro.paula@gmail.com>
2025-01-07 13:54:45 -0300

    refactor: desafio

--- cypress/e2e/formularioCadastro.cy.js
+++ cypress/e2e/formularioCadastro.cy.js
@@ -11,4 +11,12 @@ describe('Formulário Cadastro', ()=>{
       cy.getByData('botao-enviar').click()
       cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso!')
     })
+
+    it('Não deve permitir o cadastro de usuários com email e senha inválido', ()=>{
+        cy.getByData('botao-cadastro').click()
+        cy.getByData('email-input').type('moni@alura.com')
+        cy.getByData('senha-input').type('987654')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de nome é obrigatório')
+    })
   })
