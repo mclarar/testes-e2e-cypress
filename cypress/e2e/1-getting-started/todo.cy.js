1803fca2b1fc28cea8798bb2b15302231b8ec28f
mclarar <mariac.ribeiro.paula@gmail.com>
2025-01-07 10:06:44 -0300

    feat: criando comandos personalizados

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -1,4 +1,17 @@
-it('Deve renderizar corretamente o texto da seção de vantagens ', () => {
-  cy.visit('http://localhost:3000/');
-  cy.get('h2').contains('Vantagens do nosso banco:');
-});
+
+describe('Página inicial', () => {
+  beforeEach(()=>{
+    cy.visit('http://localhost:3000/');
+  })
+  it('Deve renderizar o h1 com o texto correto', () => {
+    cy.getByData('titulo-principal').contains(
+      'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+    );
+  });
+
+  it('Deve renderizar corretamente o texto da seção de vantagens ', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('h2').contains('Vantagens do nosso banco:');
+  });
+  
+})
